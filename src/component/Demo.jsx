05ae7322eae14d3146ad9e02e3b912924b516b1f
zhangjinpei <302784355@qq.com>
2016-03-07 16:30:29 +0800

    edit file

---
+++ src/component/Demo.jsx
@@ -0,0 +1,20 @@
+/**
+ * Created by peach on 16-3-7.
+ */
+import React from 'react';
+import {Form, Input, Button, message} from 'antd';
+
+class Demo extends React.Component {
+    getInitialState() {
+        return ({example: 'example'});
+    }
+
+    render() {
+        return (
+            <div>
+              准备做个像样的demo
+            </div>
+        );
+    }
+}
+export default Demo;
--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -1,6 +1,8 @@
-import '../common/lib';
-import App from '../component/App.jsx';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import '../common/lib';
+import App from '../component/App.jsx';
+import Demo from '../component/Demo.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-content'));
+//ReactDOM.render(<App />, document.getElementById('react-content'));
+ReactDOM.render(<Demo />, document.getElementById('react-content'));
