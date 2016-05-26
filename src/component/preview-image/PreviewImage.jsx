0f8c5aa675b224c3281d51d7c00c7c9c87486e1e
zhangjinpei <302784355@qq.com>
2016-05-26 19:03:27 +0800

    edit style

--- src/component/preview-image/PreviewImage.jsx
+++ src/component/preview-image/PreviewImage.jsx
@@ -12,6 +12,52 @@ class PreviewImage extends React.Component {
     super(props);
   }
 
+  componentDidMount = () => {
+    document.onmousemove = function MouseMove() {
+      function showLogin() {
+        var loginDiv = document.getElementById("loginDiv");
+        var zhezhao = document.getElementById("zhezhao");
+        var clientx = document.documentElement.clientWidth;
+        var clienty = document.documentElement.clientHeight;
+        var l_margin = (clientx - parseInt(loginDiv.style.width)) / 2;
+        var t_margin = (clienty - parseInt(loginDiv.style.height)-200) / 2
+        loginDiv.style.left = l_margin + "px";
+        loginDiv.style.top = t_margin +"px";
+        loginDiv.style.display = "block";
+        zhezhao.style.display = "block";
+      }
+      function hidLogin() {
+        var loginDiv = document.getElementById("loginDiv");
+        var zhezhao = document.getElementById("zhezhao");
+        loginDiv.style.display = "none";
+        zhezhao.style.display = "none";
+      }
+      function titleMove() {
+        var moveable = true;
+        var loginDiv = document.getElementById("loginDiv");
+        //以下变量提前设置好                        var clientX = window.event.clientX;
+        var clientY = window.event.clientY;
+        var moveTop = parseInt(loginDiv.style.top);
+        var moveLeft = parseInt(loginDiv.style.left);
+        document.onmousemove = function MouseMove() {
+          if (moveable) {
+            var y = moveTop + window.event.clientY - clientY;
+            var x = moveLeft + window.event.clientX - clientX;
+            if (x > 0 && y > 0) {
+              loginDiv.style.top = y + "px";
+              loginDiv.style.left = x + "px";
+            }
+          }
+        }
+        document.onmouseup = function Mouseup() {
+          moveable = false;
+        }
+      }
+
+
+    }
+  };
+
   // 初始化state,替代原getInitialState, 注意前面没有static
   state = {
     showPreviewImage: false,
@@ -75,9 +121,9 @@ class PreviewImage extends React.Component {
         <div className="preview-wrap" style={{display: this.state.showPreviewImage ? 'table': 'none'}}>
           <div className="preview-mask" onClick={()=>{this.handleClosePreview()}}></div>
           <div className="thumbnail-table-cell">
-            <div className="table-cell-content">
+            <div className="table-cell-content" id="table-cell-content">
               <div className="preview-img-icon-close" onClick={()=>{this.handleClosePreview()}}>
-                <FAIcon type="fa-times fa-times-circle-o"/>
+                <FAIcon type="fa-times fa-times-circle"/>
               </div>
               <img src={this.state.scaleImageSrc} alt="图片预览"/>
             </div>
